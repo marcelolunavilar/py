016ae5709ab4e2a1fd8ffa38e2091432260e29ef
marcelolunavilar <marcelovilar@berkeley.edu>
2024-01-01 01:40:58 -0800

    first commit

---
+++ progs/main2.py
@@ -0,0 +1,11 @@
+from sys import path
+
+path.append('C:\\Users\\marce\\OneDrive\\School\\py\\packages')
+
+import extra.good.best.sigma
+
+from extra.good.best.tau import FunT
+
+
+print(extra.good.best.sigma.FunS())
+print(FunT())
